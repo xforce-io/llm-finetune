8125d28d6dfe5e9aeeb9ddd1a251dda41c818a76
许鹏 <xupeng@192.168.1.3>
2023-04-29 01:00:33 +0800

    develop

---
+++ scripts/finetune_deep.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed --num_gpus=4 \
+    --deepspeed conf/deepspeed_config.json \
+    --model_name_or_path bigscience/bloomz-560m \
+    --train_file data/sample_train.txt \
+    --validation_file data/sample_eval.txt \
+    --block_size 2048 \
+    --do_train \
+    --do_eval \
+    --output_dir output/checkpoint_emb
