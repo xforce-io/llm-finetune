d8abd1411181c1ddee354953905d54a3a23a8425
许鹏 <xupeng@192.168.1.3>
2023-04-29 01:01:58 +0800

    develop

--- scripts/finetune_deep.sh
+++ scripts/finetune_deep.sh
@@ -1,4 +1,5 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3 deepspeed --num_gpus=4 \
+    finetune/finetune.py \
     --deepspeed conf/deepspeed_config.json \
     --model_name_or_path bigscience/bloomz-560m \
     --train_file data/sample_train.txt \
