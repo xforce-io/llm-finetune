38ebb3404966a2788c7bf77c8754ee73f1e8df98
许鹏 <xupeng@xupengdeMacBook-Pro.local>
2023-05-01 00:27:19 +0800

    develop

--- scripts/finetune_deep.sh
+++ scripts/finetune_deep.sh
@@ -4,8 +4,10 @@ deepspeed --num_gpus=8 \
     --model_name_or_path bigscience/bloom-560m \
     --train_file data/sample_train.txt \
     --validation_file data/sample_eval.txt \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 8 \
+    --gradient_accumulation_steps 2 \
     --block_size 1024 \
+    --weight_decay 1e-7 \
     --do_train \
     --do_eval \
     --logging_steps 50 \
