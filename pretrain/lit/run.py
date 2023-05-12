739a4e6d6b3bbc8744fc888104057b7ada4fc78a
许鹏 <xupeng@xupengdeMacBook-Pro.local>
2023-05-12 19:53:01 +0800

    develop

--- pretrain/lit/run.py
+++ pretrain/lit/run.py
@@ -1,7 +1,7 @@
 import torch
 
 from torch.utils.data import DataLoader
-from pytorch_lightning import LightningDataModule, Trainer
+from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 from pytorch_lightning.strategies import DeepSpeedStrategy
 
 import sys
@@ -34,24 +34,29 @@ class DataModule(LightningDataModule):
     def val_dataloader(self):
         return DataLoader(self.dataset.eval_dataset, batch_size=self.batch_size)
 
+class LlmModule(LightningModule):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
+        
+    def forward(self, **inputs):
+        return self.model(**inputs)
+
 def cli_main():
     args = ArgsLit()
     dataModule = DataModule(args)
-
-    model = loadPretrain(args.modelArgs)
-    
+    llmModule = LlmModule(loadPretrain(args.modelArgs))
     trainer = Trainer(
         max_epochs=args.trainArgs.num_train_epochs,
         accelerator="auto",
         devices=1 if torch.cuda.is_available() else None,
         strategy=DeepSpeedStrategy(config=args.dataArgs.deepspeed_config))
-    trainer.fit(model, datamodule=dataModule)
+    trainer.fit(llmModule, datamodule=dataModule)
     trainer.test(
-        model=model,
+        model=llmModule,
         dataLoaders=dataModule.val_dataloader(),
         ckpt_path=args.trainArgs.output_dir, 
         datamodule=dataModule)
-
 
 if __name__ == "__main__":
     cli_main()
