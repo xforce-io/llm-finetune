6e1193a25579ff1d4d6a51e4faa4dbd24a6ec997
许鹏 <xupeng@xupengdeMacBook-Pro.local>
2023-06-10 17:48:53 +0800

    develop

--- test/test.py
+++ test/test.py
@@ -3,14 +3,32 @@ from client.client import post_request
 
 kUrl = "http://127.0.0.1:5000/v1/completions"
 kPathPclue = "benchmark/pclue/pclue.jsonl"
+kBeacon = "答案"
 
 def test(filepath):
+    cntRight = 0
+    all = 0
     with open(filepath, 'r') as file:
         for line in file:
+            all += 1
+
             json_obj = json.loads(line)
             prompt = json_obj["instruction"]
             resp = post_request(kUrl, prompt)
-            print(resp)
+            text = resp["choices"][0]["text"]
+
+            idx = text.find(kBeacon)
+            if idx < 0 :
+                continue
+            
+            processed = text[idx + len(kBeacon):].replace("：", "").replace("<unk>", "").strip()
+            answer = processed[:processed.find("<")]
+            expected = json_obj["output"]
+            if answer == expected:
+                cntRight += 1
+            else :
+                print(f"expected[{expected}] actual[{answer}] wrong for '{prompt}' ")
+    print(f"overall: right[{cntRight}] ratio[{cntRight*1.0/all}]")
 
 if __name__ == "__main__" :
     test(kPathPclue)
