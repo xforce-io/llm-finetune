7a3527f7b6c9f00a632a5ae17479756f6d0e2a5c
许鹏 <xupeng@192.168.1.3>
2023-06-18 20:33:45 +0800

    develop

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,4 +1,5 @@
 python service/server.py \
+    --port=5000 \
     --tokenizer_name_or_path=bigscience/bloom-560m \
     --config_name_or_path=bigscience/bloom-560m \
     --model_name_or_path=output/checkpoint
