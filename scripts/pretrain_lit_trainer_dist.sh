e97887067e03a5479a7ed43cb996fbcc16eeca63
许鹏 <xupeng@xupengdeMacBook-Pro.local>
2023-09-01 20:08:53 +0800

    develop

--- scripts/pretrain_lit_trainer_dist.sh
+++ scripts/pretrain_lit_trainer_dist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SLURM SUBMIT SCRIPT
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --gres=gpu:8
 
@@ -9,35 +9,34 @@
 eval "$(conda shell.bash hook)"
 conda activate xfc
 
-export NCCL_SOCKET_IFNAME="eno1"
-export NCCL_IB_PCI_RELAXED_ORDERING="1"
-export NCCL_DEBUG="INFO"
 export MASTER_ADDR=10.4.119.108
 export MASTER_PORT=12314
 export TRANSFORMERS_CACHE=/mnt/data1/.cache/huggingface/hub
 export HF_HOME=/mnt/data1/.cache/huggingface
 export HF_DATASETS_CACHE=/mnt/data1/.cache/huggingface/hub/dataset
 export TORCH_EXTENSIONS_DIR=/mnt/data1/.cache/torch_extensions
-export TORCH_DISTRIBUTED_DEBUG=INFO
+export TRITON_CACHE_DIR=/mnt/data1/.cache/triton_$user
 
-CMD="python pretrain/lit/run_trainer.py --precision bf16 \
+CMD="python pretrain/lit/run_trainer.py \
+        --precision bf16 \
         --devices 8 \
-        train_micro_batch_size_per_gpu=4 \
-        accumulate_grad_batches=16 \
+        accumulate_grad_batches=256 \
+        train_micro_batch_size_per_gpu=1 \
         deepspeed_config=\"conf/deepspeed_config.json\" \
-        train_file=\"data/sample_train_10w.txt\" \
-        validation_file=\"data/sample_eval.txt\" \
-        model_name_or_path=\"/mnt/data1/dev/github/llm-finetune/output/lm7_ns_bk/\" \
-        config_name=\"decapoda-research/llama-7b-hf\" \
-        tokenizer_name=\"decapoda-research/llama-7b-hf\" \
-        block_size=1024 \
-        preprocessing_num_workers=64 \
+        train_file=\"/mnt/data1/dev/data/nb_sample.txt\" \
+        validation_file=\"/mnt/data1/dev/github/llm-finetune/data/sample_eval.txt\" \
+        model_name_or_path=\"meta-llama/Llama-2-13b-hf\" \
+        config_name=\"meta-llama/Llama-2-13b-hf\" \
+        tokenizer_name=\"meta-llama/Llama-2-13b-hf\" \
+        block_size=4096 \
+        preprocessing_num_workers=128 \
         warmup_min_lr=1e-6 \
         warmup_max_lr=3e-4 \
         warmup_num_steps=200 \
         num_train_epochs=1 \
         default_root_dir=\"/mnt/data1/dev/github/llm-finetune/\" \
-        output_dir=\"/mnt/data1/dev/github/llm-finetune/output/tmp/\" \
+        output_dir=\"/mnt/data1/dev/github/llm-finetune/output/tmp\" \
+        use_auth_token=\"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\" \
         do_train=true \
         do_eval=true"
 srun $CMD
