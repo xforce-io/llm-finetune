c78bcbf738aa9e5eae09b5607cd177024857f5e5
许鹏 <xupeng@xupengdeMacBook-Pro.local>
2023-10-07 20:41:21 +0800

    develop

--- scripts/pretrain_lit.sh
+++ scripts/pretrain_lit.sh
@@ -1,18 +1,16 @@
-MASTER_ADDR=10.4.119.108
-MASTER_PORT=12314
-TRANSFORMERS_CACHE=/mnt/data1/.cache/huggingface/hub
-HF_HOME=/mnt/data1/.cache/huggingface
-HF_DATASETS_CACHE=/mnt/data1/.cache/huggingface/hub/dataset
-TORCH_EXTENSIONS_DIR=/mnt/data1/.cache/torch_extensions
-TRITON_CACHE_DIR=/mnt/data1/.cache/triton_$user
+export MASTER_ADDR=10.4.119.108
+export MASTER_PORT=12314
+export TRANSFORMERS_CACHE=/mnt/data1/.cache/huggingface/hub
+export HF_HOME=/mnt/data1/.cache/huggingface
+export HF_DATASETS_CACHE=/mnt/data1/.cache/huggingface/hub/dataset
 
-DEVICES=8
-ACCU_GRAD_BATCH=256
-MICRO_BATCH_SIZE=1
-BLOCK_SIZE=4096
-TRAIN_FILE="/mnt/data1/dev/data/book/book_05.txt"
-BASE_MODEL="meta-llama/Llama-2-13b-hf"
-MODEL_NAME_OR_PATH="/mnt/data1/dev/github/llm-finetune/output/lm13-230819/"
-OUTPUT_DIR="/mnt/data1/dev/github/llm-finetune/output/lm13-230901/"
+export DEVICES=8
+export ACCU_GRAD_BATCH=256
+export MICRO_BATCH_SIZE=1
+export BLOCK_SIZE=4096
+export TRAIN_FILE="/mnt/data1/dev/data/book/book_05.txt"
+export BASE_MODEL="meta-llama/Llama-2-13b-hf"
+export MODEL_NAME_OR_PATH="/mnt/data1/dev/github/llm-finetune/output/lm13-230819/"
+export OUTPUT_DIR="/mnt/data1/dev/github/llm-finetune/output/lm13-230901/"
 
-bash scripts/bash_pretrain_lit.sh
+bash scripts/base_pretrain_lit.sh
--- scripts/pretrain_lit_dist.sh
+++ scripts/pretrain_lit_dist.sh
@@ -1,18 +1,18 @@
-MASTER_ADDR=10.4.119.108
-MASTER_PORT=12314
-TRANSFORMERS_CACHE=/mnt/data1/.cache/huggingface/hub
-HF_HOME=/mnt/data1/.cache/huggingface
-HF_DATASETS_CACHE=/mnt/data1/.cache/huggingface/hub/dataset
-TORCH_EXTENSIONS_DIR=/mnt/data1/.cache/torch_extensions
-TRITON_CACHE_DIR=/mnt/data1/.cache/triton_$user
+export MASTER_ADDR=10.4.119.108
+export MASTER_PORT=12314
+export TRANSFORMERS_CACHE=/mnt/data1/.cache/huggingface/hub
+export HF_HOME=/mnt/data1/.cache/huggingface
+export HF_DATASETS_CACHE=/mnt/data1/.cache/huggingface/hub/dataset
+export TORCH_EXTENSIONS_DIR=/mnt/data1/.cache/torch_extensions
+export TRITON_CACHE_DIR=/mnt/data1/.cache/triton_$user
 
-DEVICES=8
-ACCU_GRAD_BATCH=256
-MICRO_BATCH_SIZE=1
-BLOCK_SIZE=4096
-TRAIN_FILE="/mnt/data1/dev/data/nb_sample.txt"
-BASE_MODEL="meta-llama/Llama-2-13b-hf"
-MODEL_NAME_OR_PATH=$BASE_MODEL
-OUTPUT_DIR="/mnt/data1/dev/github/llm-finetune/output/tmp/"
+export DEVICES=8
+export ACCU_GRAD_BATCH=256
+export MICRO_BATCH_SIZE=1
+export BLOCK_SIZE=4096
+export TRAIN_FILE="/mnt/data1/dev/data/nb_sample.txt"
+export BASE_MODEL="meta-llama/Llama-2-13b-hf"
+export MODEL_NAME_OR_PATH=$BASE_MODEL
+export OUTPUT_DIR="/mnt/data1/dev/github/llm-finetune/output/tmp/"
 
 bash scripts/base_pretrain_lit_dist.sh
